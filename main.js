74d8b6e0a19ea58b820bb06baeca5ce3b95637e3
cicadanonymous3301-role <cicadanonymous3301@gmail.com>
2026-05-12 17:00:11 +0700

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let cart = JSON.parse(localStorage.getItem('CART_DATA')) || []
+let allProducts = []
 const navItems = document.querySelectorAll('nav ul li');
 const sections = document.querySelectorAll('section')
 const hamburger = document.querySelector('.btn-hamburger')
@@ -37,3 +39,84 @@ navItems.forEach(item => {
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
+
+async function loadProducts() {
+    try{
+        const response = await fetch('products.json')
+        allProducts = await response.json()
+        renderProducts(allProducts)
+        updateCartCount()
+    } catch(error){
+        console.error('Gagal mengambil data produk:', error)
+    }
+}
+
+function renderProducts(products){
+    const container = document.getElementById('product-container')
+    container.innerHTML = ''
+
+    products.forEach(product => {
+        const tagsHTML = product.tags.map(tag => `<span>${tag}</span>`)
+
+        const cardHTML = `
+        <article class="custom-card">
+                <div class="card-image">
+                  <span class="category-badge">${product.category}</span>
+                  <img src="${product.image}" alt="Product 01">
+                </div>
+
+                <div class="card-body">
+                  <div class="card-desc">
+                    <h3 class="card-name">${product.name}</h3>
+                    <div class="price-section">
+                    <p class="card-price">${product.price}</p>
+                    <span class="sub-price">/${product.perpfpm}  lpfpm</span>
+                    </div>
+                    <p class="card-text">${product.description}</p>
+                    <div class="card-tags">
+                      ${tagsHTML}
+                    </div>
+                  </div>
+                  <button class="cart-btn" onclick="addToCart(${product.id})">
+                        <i class="bi bi-cart-plus"></i> Add To Cart
+                    </button>
+                </div>
+              </article>
+        `
+
+        container.innerHTML += cardHTML
+    })
+}
+
+loadProducts()
+
+function addToCart(productId){
+    const product = allProducts.find(p => p.id === productId)
+
+    const existingItem = cart.find(item => item.id === productId)
+
+    if(existingItem){
+        existingItem.quantity += 1
+    } else {
+        cart.push({
+            ...product,
+            quantity: 1
+        })
+    }
+
+    saveCart()
+    updateCartCount()
+
+    console.log('Isi keranjang :', cart)
+}
+
+function saveCart(){
+    localStorage.setItem('CART_DATA', JSON.stringify(cart))
+}
+
+function updateCartCount() {
+    const countElement = document.getElementById('cart-count');
+    // Hitung total quantity semua barang di keranjang
+    const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+    countElement.innerText = totalItems;
+}
